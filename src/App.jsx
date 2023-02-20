ddd8e22fb2d3eb77270cf439b3f0ec5fb15d88ca
leandrofiadone <leanfiadone@gmail.com>
2023-02-20 17:18:01 -0300

    orden paises

--- src/App.jsx
+++ src/App.jsx
@@ -38,36 +38,78 @@ function Clock(props) {
 function App() {
 	return (
 		<div className="app">
+			<Clock label="España" timeZone="Europe/Madrid" countryCode="ES" />
+			<Clock
+				label="Guinea Ecuatorial"
+				timeZone="Africa/Malabo"
+				countryCode="GQ"
+			/>
+
+			<Clock label="Uruguay" timeZone="America/Montevideo" countryCode="UY" />
 			<Clock
 				label="Argentina"
 				timeZone="America/Argentina/Buenos_Aires"
 				countryCode="AR"
 			/>
-
-			<Clock label="Colombia" timeZone="America/Bogota" countryCode="CO" />
-			<Clock label="Ecuador" timeZone="America/Guayaquil" countryCode="EC" />
-			<Clock label="España" timeZone="Europe/Madrid" countryCode="ES" />
-			<Clock label="Venezuela" timeZone="America/Caracas" countryCode="VE" />
 			<Clock label="Chile" timeZone="America/Santiago" countryCode="CL" />
-			<Clock label="Uruguay" timeZone="America/Montevideo" countryCode="UY" />
-			<Clock label="Mexico" timeZone="America/Mexico_City" countryCode="MX" />
-			<Clock label="Peru" timeZone="America/Lima" countryCode="PE" />
-			<Clock label="Guatemala" timeZone="America/Guatemala" countryCode="GT" />
-			<Clock label="Cuba" timeZone="America/Havana" countryCode="CU" />
+			<Clock label="Paraguay" timeZone="America/Asuncion" countryCode="PY" />
 			<Clock label="Bolivia" timeZone="America/La_Paz" countryCode="BO" />
+			<Clock label="Cuba" timeZone="America/Havana" countryCode="CU" />
 			<Clock
-				label="Dominican Republic"
+				label="República Dominicana"
 				timeZone="America/Santo_Domingo"
 				countryCode="DO"
 			/>
-			<Clock label="Paraguay" timeZone="America/Asuncion" countryCode="PY" />
+			<Clock
+				label="Guinea Ecuatorial"
+				timeZone="Africa/Malabo"
+				countryCode="GQ"
+			/>
+			<Clock label="México" timeZone="America/Mexico_City" countryCode="MX" />
+			<Clock label="Honduras" timeZone="America/Tegucigalpa" countryCode="HN" />
+			<Clock label="Guatemala" timeZone="America/Guatemala" countryCode="GT" />
 			<Clock label="Nicaragua" timeZone="America/Managua" countryCode="NI" />
 			<Clock
 				label="Costa Rica"
 				timeZone="America/Costa_Rica"
 				countryCode="CR"
 			/>
-			<Clock label="Panama" timeZone="America/Panama" countryCode="PA" />
+			<Clock label="Panamá" timeZone="America/Panama" countryCode="PA" />
+			<Clock label="Colombia" timeZone="America/Bogota" countryCode="CO" />
+			<Clock label="Venezuela" timeZone="America/Caracas" countryCode="VE" />
+			<Clock label="Ecuador" timeZone="America/Guayaquil" countryCode="EC" />
+
+			<Clock label="Ecuador" timeZone="America/Guayaquil" countryCode="EC" />
+			<Clock label="Venezuela" timeZone="America/Caracas" countryCode="VE" />
+
+			<Clock label="México" timeZone="America/Mexico_City" countryCode="MX" />
+			<Clock label="Perú" timeZone="America/Lima" countryCode="PE" />
+			<Clock label="Honduras" timeZone="America/Tegucigalpa" countryCode="HN" />
+			<Clock
+				label="El Salvador"
+				timeZone="America/El_Salvador"
+				countryCode="SV"
+			/>
+			<Clock
+				label="Puerto Rico"
+				timeZone="America/Puerto_Rico"
+				countryCode="PR"
+			/>
+			<Clock label="Guatemala" timeZone="America/Guatemala" countryCode="GT" />
+			<Clock label="Nicaragua" timeZone="America/Managua" countryCode="NI" />
+			<Clock label="Cuba" timeZone="America/Havana" countryCode="CU" />
+			<Clock label="Bolivia" timeZone="America/La_Paz" countryCode="BO" />
+			<Clock
+				label="República Dominicana"
+				timeZone="America/Santo_Domingo"
+				countryCode="DO"
+			/>
+			<Clock
+				label="Costa Rica"
+				timeZone="America/Costa_Rica"
+				countryCode="CR"
+			/>
+			<Clock label="Panamá" timeZone="America/Panama" countryCode="PA" />
 		</div>
 	);
 }
